511eeb27237e2db6348b9413e668aadea1c51f97
Bcam <cam.barbara@hotmail.com>
2021-04-28 15:07:05 -0400

    files ready

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class="borderDiv"></div>
+    <nav class="footerNav">
+        <ul>
+            <li><a href="">About Us</a></li>
+            <li><a href="">FAQ</a></li>
+            <li><a href="">Terms of Service</a></li>
+        </ul>        
+    </nav>    
+    <p>©Barbara Cam. All rights reserved.</p>
+</footer>
---
+++ nav.php
@@ -0,0 +1,17 @@
+<header>
+    <div class="banner">
+        <div class="logo">
+            <img src="./images/Moviecovid1.png" alt="">
+        </div>
+        <h1 class="siteName">Covid Date</h1>
+    </div>
+    <div class="borderDiv"></div>
+    <nav>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Movies</a></li>
+            <li><a href="">Recipes</a></li>
+            <li><a href="">Things to Do</a></li>
+        </ul>
+    </nav>    
+</header>
